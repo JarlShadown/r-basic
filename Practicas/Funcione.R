c92146d27f652b77ec8c10f3c6b304018f64c76a
JarlShadown <65218889+JarlShadown@users.noreply.github.com>
2020-08-30 20:07:42 -0700

    Create Funcione.R

---
+++ Practicas/Funcione.R
@@ -0,0 +1,14 @@
+appBasic = function(a,b){
+  print("Suma")
+  print(a+b)
+  print("Resta")
+  print(a-b)
+  print("Producto")
+  print(a*b)
+  print("Division Entera")
+  print(a%/%b)
+  print("Residuo de la division")
+  print(a%%b)
+}
+
+Conj(2-2i)
